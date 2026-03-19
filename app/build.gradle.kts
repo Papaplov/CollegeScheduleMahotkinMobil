e9c8561f95d75d9fea20dd034b23eb17c3bbf43f
Stepa <stefan.mahotkin2@gmail.com>
2026-03-19 21:23:37 +0700

    Added implemenatations in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,15 @@ android {
 }
 
 dependencies {
+    // Основные зависимости проекта (уже будут в файле)
+    // ...
+    // Сетевое взаимодействие (Retrofit)
+    implementation("com.squareup.retrofit2:retrofit:2.12.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.12.0")
+    // Управление состоянием UI (ViewModel для Compose) – опционально, если ктото захочет лезть в MVVM
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    // Асинхронные операции (Корутины)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
