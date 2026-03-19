fb7942cfa19a3e04c569baa0dab9536e93a02f15
Stepa <stefan.mahotkin2@gmail.com>
2026-03-19 21:51:50 +0700

    Creating ScheduleApi.kt, RetrofitInstance.kt, and ScheduleRepository.kt in the data package

---
+++ app/src/main/java/com/example/collegeschedulemahotkinmobil/data/repository/ScheduleRepository.kt
@@ -0,0 +1,14 @@
+package com.example.collegeschedulemahotkinmobil.data.repository
+
+import com.example.collegeschedulemahotkinmobil.data.api.ScheduleApi
+import com.example.collegeschedulemahotkinmobil.data.dto.ScheduleByDateDto
+
+class ScheduleRepository(private val api: ScheduleApi) {
+    suspend fun loadSchedule(group: String): List<ScheduleByDateDto> {
+        return api.getSchedule(
+            groupName = group,
+            start = "2026-01-12",
+            end = "2026-01-17"
+        )
+    }
+}
