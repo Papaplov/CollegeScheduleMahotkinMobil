5da1595665d715e1173047537c135db36afa4373
Stepa <stefan.mahotkin2@gmail.com>
2026-03-24 16:03:47 +0700

    Created GroupDto.kt and updated ScheduleApi.kt

--- app/src/main/java/com/example/collegeschedulemahotkinmobil/data/api/ScheduleApi.kt
+++ app/src/main/java/com/example/collegeschedulemahotkinmobil/data/api/ScheduleApi.kt
@@ -1,11 +1,15 @@
 package com.example.collegeschedulemahotkinmobil.data.api
 
+import com.example.collegeschedulemahotkinmobil.data.dto.GroupDto
 import com.example.collegeschedulemahotkinmobil.data.dto.ScheduleByDateDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ScheduleApi {
+    @GET("api/schedule/groups")
+    suspend fun getAllGroups(): List<GroupDto>
+
     @GET("api/schedule/group/{groupId}")
     suspend fun getSchedule(
         @Path("groupId") groupId: Int,          // числовой ID группы (например, 1)
---
+++ app/src/main/java/com/example/collegeschedulemahotkinmobil/data/dto/GroupDto.kt
@@ -0,0 +1,8 @@
+package com.example.collegeschedulemahotkinmobil.data.dto
+
+data class GroupDto(
+    val groupId: Int,
+    val groupName: String,
+    val specialtyName: String?,
+    val course: Int?
+)
