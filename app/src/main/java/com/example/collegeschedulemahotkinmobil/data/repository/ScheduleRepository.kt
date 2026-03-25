fa895ad34fbc2ed033febe594a3e6bad50360e5e
Stepa <stefan.mahotkin2@gmail.com>
2026-03-25 18:34:24 +0700

    Updated ScheduleRepository.kt

--- app/src/main/java/com/example/collegeschedulemahotkinmobil/data/repository/ScheduleRepository.kt
+++ app/src/main/java/com/example/collegeschedulemahotkinmobil/data/repository/ScheduleRepository.kt
@@ -1,9 +1,14 @@
 package com.example.collegeschedulemahotkinmobil.data.repository
 
 import com.example.collegeschedulemahotkinmobil.data.api.ScheduleApi
+import com.example.collegeschedulemahotkinmobil.data.dto.GroupDto
 import com.example.collegeschedulemahotkinmobil.data.dto.ScheduleByDateDto
 
 class ScheduleRepository(private val api: ScheduleApi) {
+    suspend fun getAllGroups(): List<GroupDto> {
+        return api.getAllGroups()
+    }
+
     suspend fun loadSchedule(groupId: Int, groupName: String, start: String, end: String): List<ScheduleByDateDto> {
         return api.getSchedule(groupId, groupName, start, end)
     }
